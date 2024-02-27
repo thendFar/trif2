b68bde99e67b8d796817b76107bb8b736949f128
Ill12 <sleptsov2004@gmail.com>
2024-02-27 12:37:41 +0900

    Initial commit

---
+++ app/src/main/java/ru/startandroid/develop/trif2/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package ru.startandroid.develop.trif2.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
